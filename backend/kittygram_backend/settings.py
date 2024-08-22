b63aa82ded0de4abcc999ba241464bfbcd9b84dc
corbuncul <agafivan@gmai.com>
2024-08-22 22:40:14 +0300

    bugfix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv('SERVER_IP'), os.getenv('SERVER_DOMAIN')]
 
